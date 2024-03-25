9d72cebc7e5945463d0dcf52c3ae5bc558fa21e3
Davi Ferreira da Silva <sildv45@gmail.com>
2024-03-25 09:51:02 -0300

    Fiz outra parte do program

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import logoImg from "../../assets/logo.svg";
 import { Await, Link, useNavigate } from "react-router-dom";
 import { Container } from "../../components/container";
@@ -13,6 +13,7 @@ import {
   updateProfile,
   signOut,
 } from "firebase/auth";
+import { AuthContext } from "../../contexts/AuthConstext";
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome pe obrigatório"),
@@ -29,6 +30,8 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 export function Register() {
+  const { handleInfoUser } = useContext(AuthContext);
+
   const navigate = useNavigate();
   const {
     register,
@@ -51,6 +54,12 @@ export function Register() {
       .then(async (user) => {
         await updateProfile(user.user, {
           displayName: data.name,
+        });
+
+        handleInfoUser({
+          name: data.name,
+          email: data.email,
+          uid: user.user.uid,
         });
 
         console.log("CADASTRADO COM SUCESSO !");
